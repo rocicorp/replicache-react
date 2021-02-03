654646b83b6fdf25794ec97d0dc2940474432ae2
Aaron Boodman <aaron@aaronboodman.com>
2021-02-02 21:17:14 -1000

    first

---
+++ src/index.ts
@@ -0,0 +1,19 @@
+import type Replicache from 'replicache';
+import type {JSONValue, ReadTransaction} from 'replicache';
+import {useEffect, useState} from 'react';
+
+export function useSubscribe<R extends JSONValue>(
+    rep: Replicache,
+    query: (tx: ReadTransaction) => Promise<R>,
+    def: R,
+  ): R {
+  const [snapshot, setSnapshot] = useState<R>(def);
+  useEffect(() => {
+    return rep.subscribe(query, {
+      onData: (data: R) => {
+        setSnapshot(data);
+      }
+    });
+  }, [rep]);
+  return snapshot;
+}
