a5938a3061028ff7d91312061253642817aa259a
Aaron Boodman <aaron@aaronboodman.com>
2021-02-15 15:52:07 -1000

    Allow caller to specify additional dependencies.

--- src/index.ts
+++ src/index.ts
@@ -6,6 +6,7 @@ export function useSubscribe<R extends JSONValue>(
     rep: Replicache,
     query: (tx: ReadTransaction) => Promise<R>,
     def: R,
+    deps: Array<any> = [],
   ): R {
   const [snapshot, setSnapshot] = useState<R>(def);
   useEffect(() => {
@@ -14,6 +15,6 @@ export function useSubscribe<R extends JSONValue>(
         setSnapshot(data);
       }
     });
-  }, [rep]);
+  }, [rep, ...deps]);
   return snapshot;
 }
