e0f56ade87162443d491c550f167db198ea171e1
Erik Arvidsson <arv@roci.dev>
2021-05-20 14:27:34 -0700

    Make React and Replicache peer dependencies (#2)
    We also need Replicache as a devDependency because we need the .d.ts
    file for it to be able to compile.

--- src/index.ts
+++ src/index.ts
@@ -3,17 +3,15 @@ import type {JSONValue, ReadTransaction} from 'replicache';
 import {useEffect, useState} from 'react';
 
 export function useSubscribe<R extends JSONValue>(
-    rep: Replicache,
-    query: (tx: ReadTransaction) => Promise<R>,
-    def: R,
-    deps: Array<any> = [],
-  ): R {
+  rep: Replicache,
+  query: (tx: ReadTransaction) => Promise<R>,
+  def: R,
+  deps: Array<any> = [],
+): R {
   const [snapshot, setSnapshot] = useState<R>(def);
   useEffect(() => {
     return rep.subscribe(query, {
-      onData: (data: R) => {
-        setSnapshot(data);
-      }
+      onData: (data: R) => setSnapshot(data),
     });
   }, [rep, ...deps]);
   return snapshot;
