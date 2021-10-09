d7c149c6668d57db00bdec2c2c50725fc0764424
Erik Arvidsson <erik.arvidsson@gmail.com>
2021-10-08 17:19:27 -0700

    feat: Batch all subscription callbacks
    This uses `ReactDOM.unstable_batchedUpdates`
    
    Fixes #4

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,24 @@
 import type {Replicache} from 'replicache';
 import type {ReadonlyJSONValue, ReadTransaction} from 'replicache';
 import {useEffect, useState} from 'react';
+import {unstable_batchedUpdates} from 'react-dom';
+
+// We wrap all the callbacks in a `unstable_batchedUpdates` call to ensure that
+// we do not render things more than once overv all of the changed subscriptions.
+
+let hasPendingCallback = false;
+let callbacks: (() => void)[] = [];
+
+function doCallback() {
+  const cbs = callbacks;
+  callbacks = [];
+  hasPendingCallback = false;
+  unstable_batchedUpdates(() => {
+    for (const callback of cbs) {
+      callback();
+    }
+  });
+}
 
 export function useSubscribe<R extends ReadonlyJSONValue>(
   rep: Replicache,
@@ -11,7 +29,13 @@ export function useSubscribe<R extends ReadonlyJSONValue>(
   const [snapshot, setSnapshot] = useState<R>(def);
   useEffect(() => {
     return rep.subscribe(query, {
-      onData: (data: R) => setSnapshot(data),
+      onData: (data: R) => {
+        callbacks.push(() => setSnapshot(data));
+        if (!hasPendingCallback) {
+          Promise.resolve().then(doCallback);
+          hasPendingCallback = true;
+        }
+      },
     });
   }, [rep, ...deps]);
   return snapshot;
